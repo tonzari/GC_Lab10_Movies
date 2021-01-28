e5c45985042a69d27cb01916bb75e79ac06e7153
antonio <antoniomanzari@gmail.com>
2021-01-28 14:32:12 -0500

    init commit

---
+++ GC_Lab10_Movies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GC_Lab10_Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MOVIES!");
+        }
+    }
+}
